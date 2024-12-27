a5f502be813c094a595e104f1f7ce6491112182d
KRythy <kbe2004bar@gmail.com>
2024-12-27 15:07:48 +0700

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Krythy"
+title = "Заметка"
+content = "Дописать код"
+status = "Выполнено"
+created_date = "27-12-2024"
+issue_date = "28-12-2024"
+
+print('Имя пользователя:', username)
+print('Заголовок заметки:', title)
+print('Описание заметки:', content)
+print('Статус заметки:', status)
+print('Дата создания заметки:', created_date)
+print('Дата истечения заметки:', issue_date)
