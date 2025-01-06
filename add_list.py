4825a04e373a2b938a2ea7202b5aa154767f4d54
KRythy <kbe2004bar@gmail.com>
2025-01-06 08:26:28 +0700

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -1,13 +1,12 @@
-title = []
+titles = []
 
 title1 = input("Введите первый заголовок: ")
 title2 = input("Введите второй заголовок: ")
 title3 = input("Введите третий заголовок: ")
 
-title.append(title1)
-title.append(title2)
-title.append(title3)
+titles.append(title1)
+titles.append(title2)
+titles.append(title3)
 
 print("Заголовки заметок:")
-for title in title:
     print(title)
